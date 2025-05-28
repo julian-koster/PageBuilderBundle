032bd10043b13649b27242f62ab2d8d228b3e4f8
Julian Koster <julian@pcs-nl.com>
2025-05-28 10:31:15 +0200

    Remove exception on image upload dir not existing

--- src/DependencyInjection/ConfigValidator.php
+++ src/DependencyInjection/ConfigValidator.php
@@ -2,11 +2,16 @@
 
 namespace JulianKoster\PageBuilderBundle\DependencyInjection;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ConfigValidator
 {
-    public static function validate(array $config, ContainerBuilder $builder): void
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+    }
+
+    public function validate(array $config, ContainerBuilder $builder): void
     {
         $resolvedTemplateDir = $builder->getParameterBag()->resolveValue($config['template_dir']);
 
@@ -19,8 +24,9 @@ class ConfigValidator
 
         $resolvedImageDir = $builder->getParameterBag()->resolveValue($config['image_dir']);
 
+        # We're not going to throw an actual exception as it seems silly to halt the entire build process over an image upload dir.
         if (!\is_dir($resolvedImageDir)) {
-            throw new \InvalidArgumentException(sprintf(
+            $this->logger->warning(sprintf(
                 'Invalid page_builder.image_dir "%s": directory does not exist. Did you forget to create the directory? If created, make sure it\'s path matches with the path configured under page_builder.image_dir in the PageBuilderBundle\'s configuration file.',
                 $resolvedImageDir
             ));
